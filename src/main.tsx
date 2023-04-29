8f599a580bfb0a0e980466df8f2ac068e51db2cd
Vitaly Rudenko <maurice.dederick@gmail.com>
2023-04-29 20:14:03 +0300

    fix: dark theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Timestamps } from './pages/timestamps/Timestamps'
+import { ThemeProvider } from '@emotion/react'
+import { createTheme } from '@mui/material'
 
 import './index.css'
 
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+})
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    {/* <Records /> */}
-    <Timestamps />
+    <ThemeProvider theme={darkTheme}>
+      {/* <Records /> */}
+      <Timestamps />
+    </ThemeProvider>
   </React.StrictMode>,
 )
